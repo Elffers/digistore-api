f7b36524926367a82cd6a6049be2dcc7e6ffdc68
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-18 10:06:02 -0700

    cors rack

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ Bundler.require(:default, Rails.env)
 
 module DigstoreApi
   class Application < Rails::Application
+    config.middleware.use Rack::Cors do
+      allow do
+        # this allows any other domain to access your API
+        origins 'http://localhost:9000'
+        # this allows them to access any URL. Can specify which resource, i.e. /products
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
