9af74577f58e0b020349a1f41492a1a68852a617
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-20 14:50:56 -0700

    orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 DigstoreApi::Application.routes.draw do
 
   defaults format: :json do
-    # get '/products'         => 'products#index'
-    # get '/products/:id'     => 'products#show'
-
-    # get '/orders'           => 'orders#index'
-    # get '/orders/:id'       => 'orders#show'
-
-    # get '/items'            => 'items#index'
-
-    # get '/items/:id'        => 'items#show'
-
-    # get '/carts'            => 'carts#index'
-    # post '/carts'           => 'carts#create'
-
     resources :items
     resources :carts
     resources :products
