1435e5faa4c9bd51d20982980d434b5a6aecf1f2
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-22 22:23:30 -0700

    add create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,11 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def create
+    @product = Product.create(product_params)
+    render :show
+  end
+
   def show
     @product = Product.find(params[:id])
   end
@@ -12,6 +17,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @product.destroy
     render :show
+  end
+
+  private
+
+  def product_params
+    params.require('product').permit(:id, :name, :price, :description, :avatar, :image)
   end
 
 end
